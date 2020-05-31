4b6331e6468d0c3562d9431bd8141a9f1aaee666
Jyben <benjamin.letessier@gmail.com>
2020-06-01 00:29:52 +0200

    Fix des alertes

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,5 @@
 let played = false;
+let notified = false;
 
 if ('serviceWorker' in navigator) {
 	run().catch(error => console.error(error))
@@ -19,11 +20,17 @@ async function run() {
 							if (result.live) {
 								callback(result.time)
 							}
+							else if (!result.onair) {
+								notified = false;
+							}
 						});
 					})
 				},
 					60000,
 					(time) => {
+						if (notified) return;
+
+						notified = true;
 						registration.showNotification('Sardoche Live Alert', {
 							body: `Début du live de Sardoche prévu à ${time}`,
 							icon: 'https://static-cdn.jtvnw.net/emoticons/v1/300746370/2.0'
--- routes/status.js
+++ routes/status.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const sardoche = require('../services/sardoche')
 
 /* GET status. */
-router.get('/', function (req, res, next) {
-  const result = sardoche.getData()
+router.get('/', async function (req, res, next) {
+  const result = await sardoche.getData()
   res.status(200).json(result)
 });
 
