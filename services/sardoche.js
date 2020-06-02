091505a2ae30a1e32af7cf3db06bc0711fe69f61
Jyben <benjamin.letessier@gmail.com>
2020-06-02 23:58:37 +0200

    Affichage

--- services/sardoche.js
+++ services/sardoche.js
@@ -2,18 +2,19 @@ const axios = require('axios')
 const url = 'http://api.sardoche.tv/u/sardoche'
 
 exports.getData = async function () {
-    try {
-        const response = await axios.get(url)
-        const status = response.data
+	try {
+		const response = await axios.get(url)
+		const status = response.data
 
-        if (status.live.start && (new Date(status.live.start)).getTime() > Date.now()) {
-            const time = new Date(status.live.start).getTime()
-            return { 'live': true, 'time': time, 'onair': status.live.onair }
-        }
-        else {
-            return { 'live': false, 'onair': status.live.onair }
-        }
-    } catch (error) {
-        console.log(error)
-    }
+		if (status.live.start && (new Date(status.live.start)).getTime() > Date.now()) {
+			const time = new Date(status.live.start)
+			const hours = time.getHours() + 2
+			return { 'live': true, 'time': ("0" + hours).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2), 'onair': status.live.onair }
+		}
+		else {
+			return { 'live': false, 'onair': status.live.onair }
+		}
+	} catch (error) {
+		console.log(error)
+	}
 }
