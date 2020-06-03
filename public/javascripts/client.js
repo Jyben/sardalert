9699eee23933fd2151529cca3d27548ffbf1a059
Jyben <benjamin.letessier@gmail.com>
2020-06-03 23:48:15 +0200

    Utilisation de socket.io

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,38 +1,25 @@
 let played = false
 
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.getRegistrations().then(
-		function (registrations) {
-			try {
-				for (let registration of registrations) {
-					registration.unregister()
-				}
-			} catch (error) {
-				console.log(error)
+const socket = io.connect('https://sardalert.com')
+socket.on('data', (result) => {
+	if ('serviceWorker' in navigator) {
+		run().catch(error => console.error(error))
+	}
+
+	async function run() {
+		const registration = await navigator.serviceWorker.
+			register('sw.js', { scope: './' })
+
+		Notification.requestPermission(function (result) {
+			if (result === 'granted') {
+				registration.showNotification('Sardoche Live Alert', {
+					body: `Début du live de Sardoche prévu à ${result.time}`,
+					icon: 'https://static-cdn.jtvnw.net/emoticons/v1/300746370/2.0'
+				})
 			}
-		}
-	)
-
-	run().catch(error => console.error(error))
-}
-
-async function run() {
-	const registration = await navigator.serviceWorker.
-		register('sw.js', { scope: './' })
-
-	Notification.requestPermission(function (result) {
-		if (result === 'granted') {
-			if (!navigator.serviceWorker) {
-				return console.error("Service Worker not supported")
-			}
-
-			navigator.serviceWorker.ready
-				.then(registration => registration.sync.register('syncData'))
-				.then(() => console.log("Registered background sync"))
-				.catch(err => console.error("Error registering background sync", err))
-		}
-	})
-}
+		})
+	}
+})
 
 function play() {
 	const audio = document.getElementById("audio")
--- services/sardoche.js
+++ services/sardoche.js
@@ -1,20 +1,40 @@
 const axios = require('axios')
 const url = 'http://api.sardoche.tv/u/sardoche'
 
-exports.getData = async function () {
-	try {
-		const response = await axios.get(url)
-		const status = response.data
+let notified = false
 
-		if (status.live.start && (new Date(status.live.start)).getTime() > Date.now()) {
-			const time = new Date(status.live.start)
-			const hours = time.getHours() + 2
-			return { 'live': true, 'time': ("0" + hours).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2), 'onair': status.live.onair }
-		}
-		else {
-			return { 'live': false, 'onair': status.live.onair }
-		}
+async function syncData(io) {
+	try {
+		setInterval(async () => {
+			const result = await getData(io)
+			if (result.live && !notified) {
+				notified = true
+				io.emit('data', result)
+			}
+			else {
+				notified = false
+			}
+
+		}, 60000)
 	} catch (error) {
 		console.log(error)
 	}
+}
+
+async function getData(io) {
+	const response = await axios.get(url)
+	const status = response.data
+	if (status.live.start && (new Date(status.live.start)).getTime() > Date.now()) {
+		const time = new Date(status.live.start)
+		const hours = time.getHours() + 2
+		return { 'live': true, 'time': ('0' + hours).slice(-2) + ":" + ('0' + time.getMinutes()).slice(-2), 'onair': status.live.onair }
+	}
+	else {
+		return { 'live': false, 'onair': status.live.onair }
+	}
+}
+
+module.exports = {
+	syncData,
+	getData
 }
